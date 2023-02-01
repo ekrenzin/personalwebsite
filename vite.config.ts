b917d430451baffe0e8c6bdc43d3cb475cae49e7
Ean Krenzin <ean@eankrenzin.com>
2023-02-01 00:23:51 -0800

    update preprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	assetsInclude: ['**/*.glb'],
-	build: {
-		outDir: "cloudflare"
-	}
 };
 
 export default config;
