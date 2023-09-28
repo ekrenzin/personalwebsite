5487300bc52c2922f72241c3fad0f6db83b2fc51
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-09-28 09:08:21 -0700

    cleaner art page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,6 @@
 <script>
 	import '../app.css';
 	import NavBar from '$lib/components/NavBar.svelte';
-	import { page } from '$app/stores';
-
-	/** @type {import('./$types').PageData} */
-	export let data;
-
-	const defaultBackground =
-		'linear-gradient( 0deg, rgba(154, 86, 151, 0.09), rgba(154, 86, 151, 0.09) ), linear-gradient( 214.47deg, rgba(29, 107, 130, 0.09) -13.09%, rgba(4, 134, 152, 0.18) -10.93%, rgba(0, 0, 0, 0) 14.47%, rgba(27, 201, 239, 0.135) 73.4% )';
-
-	let mainBackground = defaultBackground;
-
-	page.subscribe((value) => {
-		if (!value) return;
-		const _page = value.url.pathname;
-		if (_page.includes('art')) {
-			mainBackground = '#0C090D';
-		} else {
-			mainBackground = defaultBackground;
-		}
-	});
-
-
-
 </script>
 
 <main >
