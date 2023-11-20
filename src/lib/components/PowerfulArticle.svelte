be62d4de3e2ba75379a30388b64b3688bbbafda4
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-11-20 13:37:29 -0800

    update text selection

--- src/lib/components/PowerfulArticle.svelte
+++ src/lib/components/PowerfulArticle.svelte
@@ -1,63 +1,75 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { writable } from 'svelte/store';
-    import { sendMessage, showChat } from './AiAssistant/chatStore';
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { sendMessage, showChat } from './AiAssistant/chatStore';
 
-    export let html: string;
+	export let html: string;
 
-    let showAnalysisButton = writable(false);
-    let buttonStyle = writable('');
-    let selectedText = '';
+	let showAnalysisButton = writable(false);
+	let buttonStyle = writable('');
+	let selectedText = '';
 
-    onMount(() => {
-        const articleElement = document.querySelector('.article');
+	onMount(() => {
+		const articleElement = document.querySelector('.article');
 
-        const checkSelection = (event) => {
-            const text = window.getSelection().toString();
-            if (text) {
-                selectedText = text;
-                showAnalysisButton.set(true);
-                updateButtonPosition(event);
-            } else {
-                showAnalysisButton.set(false);
-            }
-        };
+		articleElement.addEventListener('mouseup', updateButtonPosition);
+		articleElement.addEventListener('touchend', () => updateButtonPosition);
 
-        articleElement.addEventListener('mouseup', checkSelection);
-        articleElement.addEventListener('touchend', () => setTimeout(checkSelection, 500));
-    });
+		let selectionInterval = setInterval(checkSelection, 1000);
 
-    function updateButtonPosition(event) {
-        const x = 0;
-        const y = event.clientY - 50 + window.scrollY;
-        const styleString = `top: ${y}px; left: ${x}px; position: absolute;`;
-        buttonStyle.set(styleString);
-    }
+		return () => {
+			articleElement.removeEventListener('mouseup', updateButtonPosition);
+			articleElement.removeEventListener('touchend', () => updateButtonPosition);
+			clearInterval(selectionInterval);
+		};
+	});
 
-    function analyzeText() {
+	function checkSelection() {
+		const text = window.getSelection().toString();
+		if (text) {
+			selectedText = text;
+			showAnalysisButton.set(true);
+		} else {
+			showAnalysisButton.set(false);
+		}
+	}
+
+	function updateButtonPosition(event) {
+		const x = 0;
+		const y = event.clientY - 50 + window.scrollY;
+		const styleString = `top: ${y}px; left: ${x}px; position: absolute;`;
+		buttonStyle.set(styleString);
+
+		checkSelection();
+	}
+
+	function analyzeText() {
 		$showChat = true;
 		sendMessage(`Analyze my selected text: ${selectedText}`, window.location.pathname, html);
-    }
+	}
 </script>
 
 <article class="article">
-    {@html html}
+	{@html html}
 </article>
 
 {#if $showAnalysisButton}
-    <button on:click={analyzeText} class="analyze-button bg-blue-800 text-white p-4 rounded-full shadow-lg focus:outline-none hover:bg-blue-900" style={$buttonStyle}>
-        Analyze Selection with AI
-    </button>
+	<button
+		on:click={analyzeText}
+		class="analyze-button bg-blue-800 text-white p-4 rounded-full shadow-lg focus:outline-none hover:bg-blue-900"
+		style={$buttonStyle}
+	>
+		Analyze Selection with AI
+	</button>
 {/if}
 
 <style>
-    .analyze-button {
-        /* Basic styles for the button */
-        position: absolute; /* Position absolute is required for dynamic positioning */
-        z-index: 10; /* Ensure it's above other elements */
-        /* Other styling as needed */
-    }
-    
+	.analyze-button {
+		/* Basic styles for the button */
+		position: absolute; /* Position absolute is required for dynamic positioning */
+		z-index: 10; /* Ensure it's above other elements */
+		/* Other styling as needed */
+	}
 
 	article {
 		font-family: 'Times New Roman', Times, serif;
