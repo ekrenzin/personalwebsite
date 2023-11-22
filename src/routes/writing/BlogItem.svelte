dbbd7ed4958003297493fd8c270bf734417d708f
Ean Krenzin <eankrenzin@Eans-MacBook-Pro.local>
2023-11-21 21:04:52 -0800

    optimizeimage loading JIT

--- src/routes/writing/BlogItem.svelte
+++ src/routes/writing/BlogItem.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import ImageCarousel from '$lib/components/ImageCarousel.svelte';
 	import { cleanMD } from '$lib/utils/markdown';
 	export let markdownContent = {
@@ -42,46 +43,45 @@
 		}
 	}
 	function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
-}
-
-
+		return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+			(c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+		);
+	}
 </script>
 
 {#key markdownContent}
-{#if markdownContent}
-	<article use:onIntersect class="article-container" id={getID()}>
-		<div class="content-container">
-			<p>{@html cleanMD(markdownContent.preview)}</p>
-			<a href={markdownContent.url}>
-				<button class="mt-4 bg-blue-500 hover:bg-blue-900 text-white py-2 px-4 rounded">
-					Read {markdownContent.post_title}
-				</button>
-			</a>
-		</div>
-
-		{#if markdownContent.imageSources.length > 0}
-			<div class="image-preview-container">
-				<ImageCarousel imageSources={markdownContent.imageSources} />
+	{#if markdownContent}
+		<article use:onIntersect class="article-container" id={getID()}>
+			<div class="content-container">
+				<p>{@html cleanMD(markdownContent.preview)}</p>
+				<a href={markdownContent.url}>
+					<button class="mt-4 bg-blue-500 hover:bg-blue-900 text-white py-2 px-4 rounded">
+						Read {markdownContent.post_title}
+					</button>
+				</a>
 			</div>
-			
-			{/if}
+
 			{#key intersecting}
-			
-		{#if markdownContent.scripts.length > 0}
-		{#await uuidv4() then uid}
-			<div class="canvas-container" id={`canvas-${index}-${markdownContent.title}`}/>
-			{#each markdownContent.scripts as script}
-				<script src={script} id={`script-${index}-${markdownContent.title}`} ></script>
-			{/each}
-		{/await}
-		{/if}
-		{/key}
-		<slot />
-	</article>
-{/if}
+				{#if markdownContent.imageSources.length > 0}
+					<div in:fade class="image-preview-container">
+						{#if intersecting}
+							<ImageCarousel imageSources={markdownContent.imageSources} />
+						{/if}
+					</div>
+				{/if}
+
+				{#if markdownContent.scripts.length > 0}
+					{#await uuidv4() then uid}
+						<div class="canvas-container" id={`canvas-${index}-${markdownContent.title}`} />
+						{#each markdownContent.scripts as script}
+							<script src={script} id={`script-${index}-${markdownContent.title}`}></script>
+						{/each}
+					{/await}
+				{/if}
+			{/key}
+			<slot />
+		</article>
+	{/if}
 {/key}
 
 <style>
