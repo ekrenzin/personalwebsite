b26cd09a9cb9db0c9234dbdb84334edf9cc57087
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-12-02 18:54:46 -0800

    add videos code, but hide it

---
+++ src/routes/writing/videos.svelte
@@ -0,0 +1,17 @@
+<script>
+  
+    let videos = [{id:
+        "3pX-pZm4mvA?si=Xjgm6LLeesl2AwkD", 
+        title: "Rejecting Structure - The Non-Traditional Essay",
+    }
+    ]
+  </script>
+  
+  
+  <!-- {#each videos as video}
+  
+  <div class ="video-card" align="center">
+    <h3>{video.title}</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/3pX-pZm4mvA?si=Xjgm6LLeesl2AwkD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+  {/each} -->
