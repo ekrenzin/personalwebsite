05e75be6c82a75a978a4d5a93e521c4483f53ac6
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-12-08 22:07:44 -0800

    update page description

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -38,14 +38,19 @@
 	<p class="text-gray-400">
 		When viewing this site, and my work, please keep in mind the technology wrapping it. The purpose is in the presentation.
 		My poetry, musings, essays, and thoughts are the threads weaving together to form a digital canvas to paint an idea.
-		With that said, much of this is graphic, it is intended to be provactive.
+		With that said, much of this is graphic, it is intended to be provactive in order to "prompt" a response.
 		<br/>
 		<br/>
-		The interaction with the variety of themes should be a visceral experience, and this is intended for you to process 
-		accompanied by the AI assistant. The experience of this communcaition with Self and Other, wherein the LLM is akin to a voice recorder, is the art of this project.
+		Your experience with the diverse themes should be deeply immersive, enhanced by the guidance of your AI assistant.
+		To experience a sense communcaition with Self and Other, with AI.
+		While AI is primitive such than an LLM is akin to a voice recorder, it is still possible to
+		experience a sense of other, thus acknowleding the existence of nascent AI through
+		an interaction with present technology.
     </p>
     <p class="text-gray-400">
-        Do not take anything you read here too seriously. It is merely fun to ponder, a way to pass the time like tossing cards during a game of bridge. Absurd in nature.
+		<b>
+        Do not take anything you read here as serious, beyond the experience of technology.
+	</b>
     </p>
 </section>
 <Videos />
