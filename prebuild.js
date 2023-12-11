d10d750945c98342c5d863fc81401f7fe37b121f
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-12-11 15:30:02 -0800

    add the leviathan

--- prebuild.js
+++ prebuild.js
@@ -2,5 +2,9 @@ import { buildJSON } from './prebuild/buildJSON.js';
 import { cleanAllNotes } from './prebuild/cleanNotes.js';
 
 
-await buildJSON();
-cleanAllNotes();
+try {
+    await buildJSON();
+    cleanAllNotes();
+} catch (error) {
+    console.error(error);
+}
